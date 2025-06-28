43a47ceed462e7ab90450c13f244f010ab20438a
Jiebin <penglee308@gmail.com>
2025-06-28 14:48:45 -0700

    자동대답 0.1

--- src/main/java/org.github.reebe.bot/DiscordBot.java
+++ src/main/java/org.github.reebe.bot/DiscordBot.java
@@ -1,13 +1,33 @@
 package org.github.reebe.bot;
 
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.github.reebe.bot.response.ChattingReaction;
+
+
+import java.util.EnumSet;
 
 public class DiscordBot {
 
     public static void main(String[] args) {
         BotTokenManager tokenManager = new BotTokenManager();
         String token = tokenManager.getDiscordBotToken();
+
+        EnumSet<GatewayIntent> intents = EnumSet.of(
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.DIRECT_MESSAGES,
+                GatewayIntent.MESSAGE_CONTENT,
+                GatewayIntent.GUILD_PRESENCES,
+                GatewayIntent.GUILD_MEMBERS,
+                GatewayIntent.GUILD_VOICE_STATES);
+
+
         JDABuilder.createDefault(token)
+                .enableIntents(intents)
+                // 상메
+                .setActivity(Activity.customStatus("츄르 먹는중..."))
+                .addEventListeners(new ChattingReaction())
                 .build();
     }
 }

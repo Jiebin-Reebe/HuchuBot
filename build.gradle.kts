d13d8841e5000d213f070291d6c5fdedbb03efb5
Jiebin <penglee308@gmail.com>
2025-06-28 16:39:35 -0700

    노래 재생

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,25 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    maven {
+        setUrl("https://maven.lavalink.dev/releases")
+    }
+    maven {
+        setUrl("https://jitpack.io")
+    }
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
     implementation("net.dv8tion:JDA:5.6.1")
+    implementation("dev.arbjerg:lavaplayer:2.2.4")
+    implementation("dev.lavalink.youtube:common:1.11.2")
+
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
 }
 
 tasks.test {
---
+++ src/main/java/org.github.reebe.bot/music/TrackScheduler.java
@@ -0,0 +1,40 @@
+package org.github.reebe.bot.music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TrackScheduler extends AudioEventAdapter {
+    private final AudioPlayer audioPlayer;
+    private final BlockingQueue<AudioTrack> queue;
+
+    public TrackScheduler(AudioPlayer audioPlayer) {
+        this.audioPlayer = audioPlayer;
+        this.queue = new LinkedBlockingQueue<>();
+    }
+
+    public void queue(AudioTrack track) {
+        if (!this.audioPlayer.startTrack(track, true)) {
+            this.queue.offer(track);
+            System.out.println("▶ 트랙이 큐에 추가되었습니다: " + track.getInfo().title);
+        } else {
+            System.out.println("🎵 트랙이 바로 재생됩니다: " + track.getInfo().title);
+        }
+    }
+
+    @Override
+    public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+        System.out.println("⏹️ 트랙 종료됨: " + track.getInfo().title + " / 이유: " + endReason);
+        if (endReason.mayStartNext) {
+            nextTrack();
+        }
+    }
+
+    public void nextTrack() {
+        this.audioPlayer.startTrack(this.queue.poll(), false);
+    }
+}

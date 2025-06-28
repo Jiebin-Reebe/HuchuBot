0553a6b6066fa4d30ef838b1e77164e7caefbd6d
Jiebin <penglee308@gmail.com>
2025-06-28 14:27:24 -0700

    First Bot Commit

---
+++ src/main/java/org.github.reebe.bot/DiscordBot.java
@@ -0,0 +1,13 @@
+package org.github.reebe.bot;
+
+import net.dv8tion.jda.api.JDABuilder;
+
+public class DiscordBot {
+
+    public static void main(String[] args) {
+        BotTokenManager tokenManager = new BotTokenManager();
+        String token = tokenManager.getDiscordBotToken();
+        JDABuilder.createDefault(token)
+                .build();
+    }
+}
